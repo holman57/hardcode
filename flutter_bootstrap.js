6602654c16ca7b184199eee533e60fbb354e7df9
holman57 <holman57@gmail.com>
2026-09-14 21:11:56 +0000

    Deploying to gh-pages from @ holman57/hardcode@87aa82bd0f1b0ab4b6553c32de7eb9cbe5b29617 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"06a2e2a110089dff50fe635cffd2a61e1b24fb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "640798768" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3440564476" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
